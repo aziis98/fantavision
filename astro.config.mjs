7e5a6ce8ab27a073e6c4a86b7ea19f80d2612c21
Antonio De Lucreziis <antonio.delucreziis@gmail.com>
2024-04-29 19:26:52 +0200

    updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import preact from "@astrojs/preact";
+import preact from '@astrojs/preact'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
-});
+    outDir: 'out/',
+    integrations: [preact()],
+})
